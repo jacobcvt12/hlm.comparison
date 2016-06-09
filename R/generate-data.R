89d548e5109c5316dd24dcd2108400db5778738d
Jacob Carey <JCarey@telligen.com>
2016-06-09 10:39:45 -0400

    Add more doc

--- R/generate-data.R
+++ R/generate-data.R
@@ -1,9 +1,17 @@
+#' Generates random effects data
+#'
+#' Function can generate data with random intercept and fixed slope,
+#' or random intercept and slope with arbitrary fixed covariates
 #' @param clusters The number of clusters (> 1)
 #' @param cluster.size Either a vector indicating the size of each cluster or a single number which indicates the size of each cluster (same across clusters)
 #' @param params.fe A list containing the values of the fixed effects
 #' @param params.re A list contiaining the values of the random effects
 #' @param G "True" mixing distribution as a character (e.g. "normal", "gamma")
 #' @param ... Named parameters for the mixing distribution G
+#'
+#' @return data.frame
+#'
+#' @export
 create.data <- function(clusters,
                         cluster.size,
                         params.fe,
